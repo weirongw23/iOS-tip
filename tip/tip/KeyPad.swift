3e3b044c1eb19cef9a22fbd0305e4195624ba463
Weirong Wu <55777162+weirongw23@users.noreply.github.com>
2020-09-14 23:53:18 -0400

    Optional Challenge Section

---
+++ tip/tip/KeyPad.swift
@@ -0,0 +1,14 @@
+//
+//  KeyPad.swift
+//  tip
+//
+//  Created by Weirong Wu on 9/14/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class KeyPad: UIButton {
+    @IBInspectable var KeyPad:Bool = false
+}
--- tip/tip/ViewController.swift
+++ tip/tip/ViewController.swift
@@ -18,14 +18,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        billAmountTextField.text = ""
     }
-
     
-    @IBAction func onTap(_ sender: Any) {
+    var runningNumber = ""
+
+    @IBAction func numberPressed(_ sender: KeyPad) {
+        runningNumber += "\(sender.tag)"
+        billAmountTextField.text = runningNumber
+        calculateTip(KeyPad())
     }
+    
+    
+    @IBAction func dotPressed(_ sender: KeyPad) {
+        runningNumber += "."
+        billAmountTextField.text = runningNumber
+        calculateTip(KeyPad())
+    }
+    
+    @IBAction func clearPressed(_ sender: KeyPad) {
+        if billAmountTextField.text?.isEmpty == false {
+            billAmountTextField.text?.removeLast()
+        }
+        runningNumber = billAmountTextField.text ?? ""
+        calculateTip(KeyPad())
+    }
+    
     
     @IBAction func calculateTip(_ sender: Any) {
         // Get initial bill amount and calculate tips
+        // let bill = Double(billAmountTextField.text!) ?? 0
         let bill = Double(billAmountTextField.text!) ?? 0
         let tipPercentages = [0.15, 0.18, 0.20]
         
@@ -37,3 +59,4 @@ class ViewController: UIViewController {
     }
 }
 
+ 
